bdf65552e0af6b7863b36233262d0e86cc9f2ea5
thesagardahiwal <133154948+thesagardahiwal@users.noreply.github.com>
2025-03-30 22:17:46 +0530

    fixes bugs

--- src/app/(public)/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PublicLayout() {
-  return (
-    <div>PublicLayout</div>
-  )
-}
-
-export default PublicLayout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/authContext";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Nexe",
@@ -26,7 +15,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
           {children}
