25e43fcf81635a7be3ba68b7b93305df2b9c99a4
thesagardahiwal <133154948+thesagardahiwal@users.noreply.github.com>
2025-03-30 22:23:49 +0530

    fixes config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   webpack: (config) => {
     return config; // Ensure Webpack is used
   },
