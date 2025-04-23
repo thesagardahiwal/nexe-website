d32fa1fa9c1d647791a0af4dba289b8164d7107d
thesagardahiwal <133154948+thesagardahiwal@users.noreply.github.com>
2025-04-23 12:13:20 +0530

    link fix

--- src/app/(public)/advertise/page.tsx
+++ src/app/(public)/advertise/page.tsx
@@ -55,7 +55,7 @@ export default function AppDownload() {
 
         {/* Play Store Button */}
         <motion.a
-          href="https://www.upload-apk.com/en/HEcZyj38hb17P2j" // Replace with actual link to Play Store
+          href="https://expo.dev/artifacts/eas/9hknUmDfS1LwGvSygceJ7n.apk" // Replace with actual link to Play Store
           target="_blank"
           rel="noopener noreferrer"
           initial={{ opacity: 0, y: 40 }}
