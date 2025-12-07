40d66cc788eb78ff1dd4fe86172656941a272cc9
thesagardahiwal <133154948+thesagardahiwal@users.noreply.github.com>
2025-12-07 11:28:57 +0530

    fixed versions

--- src/app/proxy.ts
+++ src/app/proxy.ts
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const token = req.cookies.get("token");
   if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", req.url));
